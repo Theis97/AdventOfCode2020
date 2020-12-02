684c4cda983c2908fa15fae24a03484c08a23be0
Susan Theis <sttheis@hotmail.com>
2020-12-02 00:33:49 -0600

    Add solution for Day 2 Part 2

--- Day2/Day2.cpp
+++ Day2/Day2.cpp
@@ -12,7 +12,8 @@ int main() {
         return 0;
     }
 
-    int validCount = 0;
+    int oldValidCount = 0;
+    int newValidCount = 0;
 
     std::string line;
     while (std::getline(inputFile, line)) {
@@ -20,8 +21,8 @@ int main() {
         size_t dashPos = line.find('-');
         size_t colonPos = line.find(':');
 
-        int lowerBound = std::stoi(line.substr(0, dashPos));
-        int upperBound = std::stoi(line.substr(dashPos + 1, colonPos - dashPos - 3));
+        int lowerNum = std::stoi(line.substr(0, dashPos));
+        int upperNum = std::stoi(line.substr(dashPos + 1, colonPos - dashPos - 3));
         char letter = line[colonPos - 1];
         std::string password = line.substr(colonPos + 2);
 
@@ -32,11 +33,18 @@ int main() {
             }
         }
 
-        if (letterCount >= lowerBound && letterCount <= upperBound) {
-            validCount++;
+        if (letterCount >= lowerNum && letterCount <= upperNum) {
+            oldValidCount++;
+        }
+
+        char first = password[lowerNum - 1];
+        char second = password[upperNum - 1];
+        if (first == letter && second != letter || first != letter && second == letter) {
+            newValidCount++;
         }
     }
 
-    std::cout << "# Valid passwords: " << validCount << "\n";
+    std::cout << "# Valid passwords (old policy): " << oldValidCount << "\n";
+    std::cout << "# Valid passwords (new policy): " << newValidCount << "\n";
     inputFile.close();
 }
