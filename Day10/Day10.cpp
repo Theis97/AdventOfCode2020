cc9209b1eb6c6a85fa69f617684e81981cbaddcb
Susan Theis <sttheis@hotmail.com>
2020-12-14 22:07:38 -0600

    Add Day 10 part 2 solution

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -5,9 +5,15 @@
 #include <algorithm>
 
 
-int CountCombinations(std::vector<int> chunk) {
-    // TODO
-    return -1;
+double ComputeCombos(int oneDiffCount) {
+    // This assumes that there are no more than 5 differences of one in a row
+    if (oneDiffCount == 1) {
+        return 1.0;
+    }
+    if (oneDiffCount <= 3 ) {
+        return 2.0 * ((double)oneDiffCount - 1);
+    }
+    return pow(2.0, oneDiffCount - 1) - 1;
 }
 
 int main() {
@@ -30,19 +36,31 @@ int main() {
     int prevAdaptor = 0;
     int oneDiffCount = 0;
     int threeDiffCount = 0;
+    int consecutiveOnes = 0;
+    double numCombinations = 1;
     for (int i = 0; i < adaptors.size(); i++) {
         int adaptor = adaptors[i];
         int difference = adaptor - prevAdaptor;
         if (difference == 1) {
             oneDiffCount++;
+            consecutiveOnes++;
         }
         else if (difference == 3) {
             threeDiffCount++;
+            if (consecutiveOnes != 0) {
+                numCombinations *= ComputeCombos(consecutiveOnes);
+            }
+            consecutiveOnes = 0;
         }
         prevAdaptor = adaptor;
     }
 
-
+    if (consecutiveOnes != 0) {
+        numCombinations *= ComputeCombos(consecutiveOnes);
+    }
+    
     std::cout << "There are " << oneDiffCount << " 1-gaps and " << threeDiffCount + 1 << " 3-gaps\n";
     std::cout << "Answer is: " << oneDiffCount * (threeDiffCount + 1) << "\n";
+
+    std::cout << "There are " << (long long)numCombinations << " distinct arrangements of adaptors\n";
 }
